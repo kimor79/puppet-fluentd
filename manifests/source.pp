777f72370a04d47f45c8e3d7b07b17b7d5a8c8ee
Kimo Rosenbaum <kimor79@yahoo.com>
2015-01-07 14:06:53 -0800

    Allow setting order here as well

--- manifests/source.pp
+++ manifests/source.pp
@@ -7,11 +7,13 @@ define fluentd::source (
     $time_format  = false,
     $config       = {},
     $pattern      = [],
+    $order        = undef,
 ) {
 
     concat::fragment { "source_${title}":
         target  => "/etc/td-agent/config.d/${configfile}.conf",
         require => Package["${fluentd::package_name}"],
         content => template('fluentd/source.erb'),
+        order   => $order,
     }
 }
